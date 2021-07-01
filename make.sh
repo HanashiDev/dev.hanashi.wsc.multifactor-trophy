396b798573beec39f6ca15c504acc291e83a8aca
Peter Lohse <github@hsrvmail.net>
2021-07-01 20:23:33 +0200

    packing verbessert

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -f dev.hanashi.wsc.multifactor-trophy.tar
-7z a -ttar -mx=9 dev.hanashi.wsc.multifactor-trophy.tar ./* -x!dev.hanashi.wsc.multifactor-trophy.tar -x!.git -x!.gitignore -x!make.sh
+7z a -ttar -mx=9 dev.hanashi.wsc.multifactor-trophy.tar ./* -x!dev.hanashi.wsc.multifactor-trophy.tar -x!.git -x!.gitignore -x!make.sh -x!LICENSE -x!README.md
